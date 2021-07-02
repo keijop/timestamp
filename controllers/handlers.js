dd32fb11ef74c6bd2c3acbeda17c4c64168a4794
keijop <76419484+keijop@users.noreply.github.com>
2021-07-02 11:12:44 +0300

    new handler func

--- controllers/handlers.js
+++ controllers/handlers.js
@@ -7,18 +7,21 @@ const {
 
 
 const dateResponse = (req,res) => {
-	const { date } = req.params
-	console.log(date)
-	let dateInMs = Date.parse(date)
-	let dateInUTC = millisecondsToDate(date)
-	if(!dateInMs && !dateInUTC){
-		return res.status(200).json({ error : "Invalid Date" })
+	let { date } = req.params
+
+
+	if ( millisecondsToDate(Number(date)) === 'Invalid Date'
+		&& !Date.parse( date ) ) {
+		return res.status(400).json({ error : "Invalid Date" })
 	}
-	if(!dateInUTC){ 
-	return res.status(200).json({ unix : dateInMs, utc : millisecondsToDate(dateInMs)})
+
+	if ( millisecondsToDate(Number(date)) === 'Invalid Date' ) {
+		return res.status(200).json( { unix : Date.parse( date ) , utc : new Date(date).toGMTString() })
+	} else {
+		return res.status(200).json( { unix : date, utc : new Date(Number(date)).toGMTString() })
 	}
-	res.status(200).json({ unix : date, utc : dateInUTC})
-};
+
+}
 
 const noParamResponse = (req, res) => {
 	res.status(200).json({unix : currentTimeInMilliSeconds, utc : currentUTC})
