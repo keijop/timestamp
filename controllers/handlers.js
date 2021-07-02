54b300f83a34b66018a4eca7d04b66019e37a133
keijop <76419484+keijop@users.noreply.github.com>
2021-07-02 11:20:27 +0300

    Update handlers.js

--- controllers/handlers.js
+++ controllers/handlers.js
@@ -24,7 +24,7 @@ const dateResponse = (req,res) => {
 }
 
 const noParamResponse = (req, res) => {
-	res.status(200).json({unix : currentTimeInMilliSeconds, utc : currentUTC})
+	res.status(200).json({unix : new Date().getTime(), utc : new Date().toGMTString()})
 }
 
 
